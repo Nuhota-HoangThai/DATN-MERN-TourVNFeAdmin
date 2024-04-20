f656d94f14bc675f3390ff3ecf965a9474bbb19a
Nuhota-HoangThai <nguyenhoangthai0134@gmail.com>
2024-04-20 10:53:11 +0700

    final

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,16 @@ import { store, persistor } from "./redux/store/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
           <App />
+          <ToastContainer />
         </BrowserRouter>
       </PersistGate>
     </Provider>
